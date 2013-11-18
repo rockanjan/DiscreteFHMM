b5de7a85594b8f723b1aac2457f72579b6fafb52
Anjan Nepal <rockanjan@gmail.com>
2013-11-18 17:05:48 -0500

    more minor refactoring

--- src/program/Main.java
+++ src/program/Main.java
@@ -30,19 +30,43 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		//populateBrownInfo();
 		corpus = new Corpus("\\s+", Config.vocabThreshold);
-		//trainContinueFromIndependentHMM("out/model/brown_baumwelch_10states");
+		//loadFromIndependentHMM("out/model/brown_baumwelch_10states");
+		//loadFromDifferentLayerHMM(nrLayers);
+		
 		lastIter = LastIter.read();
 		if(lastIter < 0) {
-			trainNew();
+			initializeNewModel();
 		} else {
-			String filename = "variational_model_layers_" + Config.nrLayers + 
-					"_states_" + Config.numStates + 
-					"_iter_" + lastIter + ".txt";
-			//checkTestPerplexity(filename);
-			trainContinue(filename);
+			load();
+			//checkTestPerplexity();
 		}
 		
-		
+		Config.printParams();
+		train();
+		saveModel();
+		test();
+	}
+	
+	public static void readData() throws IOException {
+		corpus.readTrain(Config.baseDirData + Config.trainFile);
+		if(Config.testFile != null && !Config.testFile.equals("")) {
+			corpus.readTest(Config.baseDirData + Config.testFile);
+		}
+		if(Config.devFile != null && !Config.devFile.equals("")) {
+			corpus.readDev(Config.baseDirData + Config.devFile);
+		}
+	}
+	
+	public static void train() throws FileNotFoundException {
+		EM em = new EM(Config.numIter, corpus, model);
+		em.start();		
+	}
+	
+	public static void saveModel() {
+		model.saveModel();
+	}
+	
+	public static void test() {
 		if(Corpus.testInstanceList != null) {
 			testVariational(model, Corpus.testInstanceList, Config.outFileTest);
 		} else {
@@ -50,111 +74,54 @@ public class Main {
 		}
 	}
 	
-	public static void populateBrownInfo() throws IOException {
-		String file = "brown-brown-cluster.txt";
-		wordToCluster = new HashMap<String, String>();
-		clusterToWords = new HashMap<String, HashSet<String>>();
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line;
-		while( (line = br.readLine()) != null) {
-			String[] splitted = line.trim().split("\\s+");
-			String cluster = splitted[0];
-			String word = splitted[1];
-			wordToCluster.put(word, cluster);
-			if(! clusterToWords.containsKey(cluster)) {
-				HashSet<String> temp = new HashSet<String>();
-				temp.add(word);
-				clusterToWords.put(cluster, temp);
-			} else {
-				clusterToWords.get(cluster).add(word);
-			}
-		}
-		//view one cluster
-		for( String word : clusterToWords.get(wordToCluster.get("million"))) {
-			System.out.print(word + " ");
-		}
-		System.out.println("Total clusters = " + clusterToWords.size());
-		br.close();
-	}
-
-	public static void trainNew() throws IOException {
+	public static void initializeNewModel() throws IOException {
 		corpus.readVocab(Config.baseDirData + Config.vocabFile);
 		Corpus.corpusVocab.get(0).writeDictionary(Config.baseDirModel + "vocab.txt");
 		corpus.setupSampler();
-		corpus.readTrain(Config.baseDirData + Config.trainFile);
-		if(Config.testFile != null && !Config.testFile.equals("")) {
-			corpus.readTest(Config.baseDirData + Config.testFile);
-		}
-		if(Config.devFile != null && !Config.devFile.equals("")) {
-			corpus.readDev(Config.baseDirData + Config.devFile);
-		}
 		model = new HMMNoFinalStateLog(Config.nrLayers, Config.numStates, corpus);
 		corpus.model = model;
 		//random init
 		model.initializeRandom(Config.random);
-		//model.param.weights.initializeZeros();
-		//model.initializeZeros();
-		model.initializeZerosToBest();
-		Config.printParams();
-		EM em = new EM(Config.numIter, corpus, model);
-		em.start();
-		model.saveModel();
+		model.initializeZerosToBest();		
 	}
 	
-	public static void trainContinueFromIndependentHMM(String folder) throws IOException {
-		corpus = new Corpus("\\s+", Config.vocabThreshold);
+	public static void loadFromIndependentHMM(String folder) throws IOException {
 		model = new HMMNoFinalStateLog(Config.nrLayers, Config.numStates, corpus);
 		model.loadModelsFromIndependentHMM(folder);
 		corpus.model = model;
-		corpus.readTrain(Config.baseDirData + Config.trainFile);
-		if(Config.testFile != null && !Config.testFile.equals("")) {
-			corpus.readTest(Config.baseDirData + Config.testFile);
-		}
-		if(Config.devFile != null && !Config.devFile.equals("")) {
-			corpus.readDev(Config.baseDirData + Config.devFile);
-		}
-		model.initializeZerosToBest();
-		Config.printParams();
-		EM em = new EM(Config.numIter, corpus, model);
-		em.start();
-		model.saveModel();
+		model.initializeZerosToBest();		
 	}
 
-	public static void trainContinue(String filename) throws IOException {
-		corpus = new Corpus("\\s+", Config.vocabThreshold);
+	public static void load() throws IOException {
 		model = new HMMNoFinalStateLog(Config.nrLayers, Config.numStates, corpus);
-		//load model for continuing training
+		String filename = "variational_model_layers_" + Config.nrLayers + 
+				"_states_" + Config.numStates + 
+				"_iter_" + lastIter + ".txt";
 		model.loadModel(filename);		
 		corpus.model = model;
-		corpus.readTrain(Config.baseDirData + Config.trainFile);
-		if(Config.testFile != null && !Config.testFile.equals("")) {
-			corpus.readTest(Config.baseDirData + Config.testFile);
-		}
-		if(Config.devFile != null && !Config.devFile.equals("")) {
-			corpus.readDev(Config.baseDirData + Config.devFile);
-		}
 		model.initializeZerosToBest();
-		Config.printParams();
-		EM em = new EM(Config.numIter, corpus, model);
-		em.start();
-		model.saveModel();
 	}
 	
-	public static void checkTestPerplexity(String filename) throws IOException {
-		corpus = new Corpus("\\s+", Config.vocabThreshold);
+	public static void loadFromDifferentLayerHMM(int nrLayers) throws IOException {
 		model = new HMMNoFinalStateLog(Config.nrLayers, Config.numStates, corpus);
-		//load model for continuing training
-		model.loadModel(filename);
-		//model.loadModelsFromIndependentHMM("out/model/softEM");
+		
+		/*** load other model ***/
+		HMMBase otherModel = new HMMNoFinalStateLog(nrLayers, Config.nrLayers, corpus);
+		String filename = "variational_model_layers_" + nrLayers + 
+				"_states_" + Config.numStates + 
+				//"_iter_" + lastIter + ".txt";
+				"_final.txt";
+		otherModel.loadModel(filename);
+		model.initializeRandom(Config.random);
+		model.param.cloneFromDifferentLayerModel(otherModel.param);
+		otherModel = null;
+		/*** other model complete ***/
+		
 		corpus.model = model;
-		corpus.readTrain(Config.baseDirData + Config.trainFile);
-		if(Config.testFile != null && !Config.testFile.equals("")) {
-			corpus.readTest(Config.baseDirData + Config.testFile);
-		}
-		if(Config.devFile != null && !Config.devFile.equals("")) {
-			corpus.readDev(Config.baseDirData + Config.devFile);
-		}
 		model.initializeZerosToBest();
+	}
+	
+	public static void checkTestPerplexity() throws IOException {
 		HMMPowModel powModel = new HMMPowModel(model);
 		System.out.println("Power model created");
 		if(Corpus.testInstanceList != null) {
@@ -169,7 +136,10 @@ public class Main {
 			varLL = varLL / Corpus.testInstanceList.numberOfTokens;
 			double perplexity = Math.pow(2, -varLL/Math.log(2));
 			System.out.println("varLL = " + varLL + " perplexity = " + perplexity);
+		} else {
+			throw new RuntimeException("Test instances not loaded for checking perplexity");
 		}
+		 
 	}
 
 	public static void testVariational(HMMBase model, InstanceList instanceList, String outFile) {
@@ -229,5 +199,32 @@ public class Main {
 		InstanceList.featurePartitionCache = null;
 		System.out.println("Finished decoding");
 		System.out.println("Total decoding time : " + decodeTiming.stop());
+	}
+	
+	public static void populateBrownInfo() throws IOException {
+		String file = "brown-brown-cluster.txt";
+		wordToCluster = new HashMap<String, String>();
+		clusterToWords = new HashMap<String, HashSet<String>>();
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		while( (line = br.readLine()) != null) {
+			String[] splitted = line.trim().split("\\s+");
+			String cluster = splitted[0];
+			String word = splitted[1];
+			wordToCluster.put(word, cluster);
+			if(! clusterToWords.containsKey(cluster)) {
+				HashSet<String> temp = new HashSet<String>();
+				temp.add(word);
+				clusterToWords.put(cluster, temp);
+			} else {
+				clusterToWords.get(cluster).add(word);
+			}
+		}
+		//view one cluster
+		for( String word : clusterToWords.get(wordToCluster.get("million"))) {
+			System.out.print(word + " ");
+		}
+		System.out.println("Total clusters = " + clusterToWords.size());
+		br.close();
 	}
 }
